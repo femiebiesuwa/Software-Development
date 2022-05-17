de47c62047f20cf06da72be9bbb361b6982b2c78
Femi <85490740+femiebiesuwa@users.noreply.github.com>
2022-05-17 21:34:10 +0100

    update

--- Week 6/Kelvin Working Conditions.js
+++ Week 6/Kelvin Working Conditions.js
@@ -5,23 +5,23 @@ let goToWork = "false"; // Declaration of goToWork variable
 if(temperature < 10){} // True condition
     if (condition == "sunny"||"cloudy"||"raining"||"snowing"||"thunder"){} // True condition
     if (goToWork == "false"){} // False condition
-    console.log("The weather is cold to go to work in."); 
+    console.log("The weather is cold, stay at work"); 
 }
 else if (temperature >= 10){} // True condition
     if (temperature <20){} // True condition
     if (condition == "sunny"||"cloudy"){} // True condition
     if (goToWork == "true"){} // True condition
-    console.log("You can go to work as the weather is nice.");
+    console.log("You can go to work as the weather is nice");
 }
 else if(temperature >= 20){} // True condition
 if (temperature <30){} 
     if (condition == "sunny"||"cloudy"||"raining"){}
     if (goToWork == "true"){}
-    console.log("You can go to work as the weather is nice too."); 
+    console.log("The weather is nice, you can go to work"); 
 }
 else if(temperature >= 30){} 
     if (condition == "sunny"||"cloudy"||"raining" ||"snowing" ||"thunder"){}
     if (goToWork == "false"){} // False condition
-    console.log("The weather is too hot to go to work in."); 
+    console.log("The weather is too hot for you to go to work"); 
 }
 
